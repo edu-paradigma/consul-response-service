a5a4891e04f4f51efa9182e7611151a34ad735e7
Eduardo <egonzalez@paradigmadigital.com>
2017-11-28 21:31:47 +0100

    Delete imports

--- src/main/java/es/paradigmadigital/consulresponse/ConsulResponseApplication.java
+++ src/main/java/es/paradigmadigital/consulresponse/ConsulResponseApplication.java
@@ -3,9 +3,6 @@ package es.paradigmadigital.consulresponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.refresh.ContextRefresher;
-import org.springframework.cloud.endpoint.RefreshEndpoint;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
